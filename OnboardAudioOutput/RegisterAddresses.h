7305a5d0569c6987b5e4842e94074b52841c6bc7
Terence Darwen <tmdarwen@gmail.com>
2017-10-18 17:45:42 -0500

    Minor formatting changes

--- OnboardAudioOutput/RegisterAddresses.h
+++ OnboardAudioOutput/RegisterAddresses.h
@@ -1,48 +1,48 @@
-#define PWR_BASE_ADDRESS	0x40007000
-#define PWR_CR				PWR_BASE_ADDRESS + 0x00
+#define PWR_BASE_ADDRESS			0x40007000
+#define PWR_CR						PWR_BASE_ADDRESS + 0x00
 
-#define RCC_BASE_ADDRESS	0x40023800
-#define RCC_CR				RCC_BASE_ADDRESS + 0x00
-#define RCC_PLLCFGR			RCC_BASE_ADDRESS + 0x04
-#define RCC_CFGR			RCC_BASE_ADDRESS + 0x08
-#define RCC_CIR				RCC_BASE_ADDRESS + 0x0C
-#define RCC_APB1RSTR		RCC_BASE_ADDRESS + 0x20  // RCC APB1 peripheral reset register for (pg 112)
-#define RCC_AHB1ENR			RCC_BASE_ADDRESS + 0x30  // RCC AHB1 peripheral clock enable register (pg 116)
-#define RCC_APB1ENR			RCC_BASE_ADDRESS + 0x40
-#define RCC_PLLI2SCFGR		RCC_BASE_ADDRESS + 0x84  // RCC PLLI2S configuration register (pg 132)
+#define RCC_BASE_ADDRESS			0x40023800
+#define RCC_CR						RCC_BASE_ADDRESS + 0x00
+#define RCC_PLLCFGR					RCC_BASE_ADDRESS + 0x04
+#define RCC_CFGR					RCC_BASE_ADDRESS + 0x08
+#define RCC_CIR						RCC_BASE_ADDRESS + 0x0C
+#define RCC_APB1RSTR				RCC_BASE_ADDRESS + 0x20  // RCC APB1 peripheral reset register for (pg 112)
+#define RCC_AHB1ENR					RCC_BASE_ADDRESS + 0x30  // RCC AHB1 peripheral clock enable register (pg 116)
+#define RCC_APB1ENR					RCC_BASE_ADDRESS + 0x40
+#define RCC_PLLI2SCFGR				RCC_BASE_ADDRESS + 0x84  // RCC PLLI2S configuration register (pg 132)
 
-#define GPIOA_BASE_ADDRESS  0x40020000
-#define GPIOA_MODER			GPIOA_BASE_ADDRESS + 0x00 // GPIO port mode register
-#define GPIOA_OTYPER		GPIOA_BASE_ADDRESS + 0x04 // GPIO port output type register
-#define GPIOA_OSPEEDR		GPIOA_BASE_ADDRESS + 0x08 // GPIO port output speed register
-#define GPIOA_PUPDR			GPIOA_BASE_ADDRESS + 0x0C // GPIO port pull-up/pull-down register
-#define GPIOA_BSRR			GPIOA_BASE_ADDRESS + 0x18 // GPIO port bit set/reset register
-#define GPIOA_AFLR			GPIOA_BASE_ADDRESS + 0x20 // GPIO alternate function low register
-#define GPIOA_AFLH			GPIOA_BASE_ADDRESS + 0x24 // GPIO alternate function high register
+#define GPIOA_BASE_ADDRESS  		0x40020000
+#define GPIOA_MODER					GPIOA_BASE_ADDRESS + 0x00 // GPIO port mode register
+#define GPIOA_OTYPER				GPIOA_BASE_ADDRESS + 0x04 // GPIO port output type register
+#define GPIOA_OSPEEDR				GPIOA_BASE_ADDRESS + 0x08 // GPIO port output speed register
+#define GPIOA_PUPDR					GPIOA_BASE_ADDRESS + 0x0C // GPIO port pull-up/pull-down register
+#define GPIOA_BSRR					GPIOA_BASE_ADDRESS + 0x18 // GPIO port bit set/reset register
+#define GPIOA_AFLR					GPIOA_BASE_ADDRESS + 0x20 // GPIO alternate function low register
+#define GPIOA_AFLH					GPIOA_BASE_ADDRESS + 0x24 // GPIO alternate function high register
 
-#define GPIOB_BASE_ADDRESS  0x40020400
-#define GPIOB_MODER			GPIOB_BASE_ADDRESS + 0x00 // GPIO port mode register
-#define GPIOB_OTYPER		GPIOB_BASE_ADDRESS + 0x04 // GPIO port output type register
-#define GPIOB_OSPEEDR		GPIOB_BASE_ADDRESS + 0x08 // GPIO port output speed register
-#define GPIOB_PUPDR			GPIOB_BASE_ADDRESS + 0x0C // GPIO port pull-up/pull-down register
-#define GPIOB_BSRR			GPIOB_BASE_ADDRESS + 0x18 // GPIO port bit set/reset register
-#define GPIOB_AFLR			GPIOB_BASE_ADDRESS + 0x20 // GPIO alternate function low register
-#define GPIOB_AFLH			GPIOB_BASE_ADDRESS + 0x24 // GPIO alternate function high register
+#define GPIOB_BASE_ADDRESS  		0x40020400
+#define GPIOB_MODER					GPIOB_BASE_ADDRESS + 0x00 // GPIO port mode register
+#define GPIOB_OTYPER				GPIOB_BASE_ADDRESS + 0x04 // GPIO port output type register
+#define GPIOB_OSPEEDR				GPIOB_BASE_ADDRESS + 0x08 // GPIO port output speed register
+#define GPIOB_PUPDR					GPIOB_BASE_ADDRESS + 0x0C // GPIO port pull-up/pull-down register
+#define GPIOB_BSRR					GPIOB_BASE_ADDRESS + 0x18 // GPIO port bit set/reset register
+#define GPIOB_AFLR					GPIOB_BASE_ADDRESS + 0x20 // GPIO alternate function low register
+#define GPIOB_AFLH					GPIOB_BASE_ADDRESS + 0x24 // GPIO alternate function high register
 
-#define GPIOC_BASE_ADDRESS  0x40020800
-#define GPIOC_MODER			GPIOC_BASE_ADDRESS + 0x00 // GPIO port mode register
-#define GPIOC_OTYPER		GPIOC_BASE_ADDRESS + 0x04 // GPIO port output type register
-#define GPIOC_OSPEEDR		GPIOC_BASE_ADDRESS + 0x08 // GPIO port output speed register
-#define GPIOC_PUPDR			GPIOC_BASE_ADDRESS + 0x0C // GPIO port pull-up/pull-down register
-#define GPIOC_BSRR			GPIOC_BASE_ADDRESS + 0x18 // GPIO port bit set/reset register
-#define GPIOC_AFLR			GPIOC_BASE_ADDRESS + 0x20 // GPIO alternate function low register
-#define GPIOC_AFLH			GPIOC_BASE_ADDRESS + 0x24 // GPIO alternate function high register
+#define GPIOC_BASE_ADDRESS  		0x40020800
+#define GPIOC_MODER					GPIOC_BASE_ADDRESS + 0x00 // GPIO port mode register
+#define GPIOC_OTYPER				GPIOC_BASE_ADDRESS + 0x04 // GPIO port output type register
+#define GPIOC_OSPEEDR				GPIOC_BASE_ADDRESS + 0x08 // GPIO port output speed register
+#define GPIOC_PUPDR					GPIOC_BASE_ADDRESS + 0x0C // GPIO port pull-up/pull-down register
+#define GPIOC_BSRR					GPIOC_BASE_ADDRESS + 0x18 // GPIO port bit set/reset register
+#define GPIOC_AFLR					GPIOC_BASE_ADDRESS + 0x20 // GPIO alternate function low register
+#define GPIOC_AFLH					GPIOC_BASE_ADDRESS + 0x24 // GPIO alternate function high register
 
-#define GPIOD_BASE_ADDRESS  0x40020C00
-#define GPIOD_MODER			GPIOD_BASE_ADDRESS + 0x00 // GPIO port mode register
-#define GPIOD_OSPEEDR		GPIOD_BASE_ADDRESS + 0x08 // GPIO port output speed register
-#define GPIOD_PUPDR			GPIOD_BASE_ADDRESS + 0x0C // GPIO port pull-up/pull-down register
-#define GPIOD_BSRR			GPIOD_BASE_ADDRESS + 0x18 // GPIO port bit set/reset register
+#define GPIOD_BASE_ADDRESS			0x40020C00
+#define GPIOD_MODER					GPIOD_BASE_ADDRESS + 0x00 // GPIO port mode register
+#define GPIOD_OSPEEDR				GPIOD_BASE_ADDRESS + 0x08 // GPIO port output speed register
+#define GPIOD_PUPDR					GPIOD_BASE_ADDRESS + 0x0C // GPIO port pull-up/pull-down register
+#define GPIOD_BSRR					GPIOD_BASE_ADDRESS + 0x18 // GPIO port bit set/reset register
 
 #define SPI3_I2S3_BASE_ADDRESS		0x40003C00
 #define SPI3_I2S3_SR				SPI3_I2S3_BASE_ADDRESS + 0x08 // SPI status register (pg 605)
