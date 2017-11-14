306a87bcbc97a6a37e059d251852696511cbcd50
Terence Darwen <tmdarwen@gmail.com>
2017-11-14 16:35:00 -0600

    Adding UARTBlockingTransfer

---
+++ UARTBlockingTransfer/main.c
@@ -0,0 +1,71 @@
+#define RCC_BASE_ADDRESS            0x40023800
+#define RCC_AHB1ENR                 RCC_BASE_ADDRESS + 0x30  // RCC AHB1 peripheral clock enable register (pg 116)
+#define RCC_APB1ENR                 RCC_BASE_ADDRESS + 0x40  // RCC APB1 peripheral clock enable register (pg 117)
+
+#define GPIOA_BASE_ADDRESS          0x40020000
+#define GPIOA_MODER                 GPIOA_BASE_ADDRESS + 0x00 // GPIO port mode register
+#define GPIOA_OSPEEDR               GPIOA_BASE_ADDRESS + 0x08 // GPIO port output speed register
+#define GPIOA_PUPR                  GPIOA_BASE_ADDRESS + 0x0C // GPIO port pull-up/pull-down register
+#define GPIOA_ODR                   GPIOA_BASE_ADDRESS + 0x14 // GPIO port output data register
+#define GPIOA_AFRL                  GPIOA_BASE_ADDRESS + 0x20 // GPIO alternate function low register
+
+#define USART2_BASE_ADDRESS          0x40004400
+#define USART2_SR                    USART2_BASE_ADDRESS + 0x00 // Status register
+#define USART2_DR                    USART2_BASE_ADDRESS + 0x04 // Data register
+#define USART2_BRR                   USART2_BASE_ADDRESS + 0x08 // Baud rate register
+#define USART2_CR1                   USART2_BASE_ADDRESS + 0x0C // Control register 1
+
+#define ACCESS(address)            *((volatile unsigned int*)(address))
+
+void SendString(char* string)
+{
+	while(*string)
+	{
+		// See pg 548 of the ST RM0383 document for more info on the USART registers.
+		while((ACCESS(USART2_SR) & (1 << 6)) == 0 || (ACCESS(USART2_SR) & (1 << 7)) == 0);  // Wait until transmission is complete
+		ACCESS(USART2_DR) &= ~(0xFF); // Clear the data register
+		ACCESS(USART2_DR) = *string; // Write the next character
+		string++;  // Advance the pointer to the next character
+	}
+}
+
+int main(void)
+{
+	// Give a clock to port A as we'll be using one of its pins for transfer of data.
+	ACCESS(RCC_AHB1ENR) |= 1;
+
+	// See pg 19 of the ST UM1842 document.  We'll be using USART2.  USART2 TX occurs on
+	// PA2 so we set this pin to alternate mode.
+	ACCESS(GPIOA_MODER) |= (1 << 5);
+
+	// See pg 149 of ST RM0383 document.  USART2 is AF7.  And pg 160 of the same document
+	// shows alternate function for pin 2 is set using alternate function low register bits
+	// 8-thru-11.
+	ACCESS(GPIOA_AFRL) |= (7 << 8);
+
+	// Set PA2 to high speed
+	ACCESS(GPIOA_OSPEEDR) |= (3 << 4);
+
+	// Give a clock USART2.  See pg 117.
+	ACCESS(RCC_APB1ENR) |= (1 << 17);
+
+	// Here we set the baud rate.  This is explained on 519 of ST RM0383.  The peripheral
+	// clock is 16 MHz by default.  So, the calculation for the USARTDIV is:
+	// DesiredBaudRate = 16MHz / 16*USARTDIV
+	// Note that the fractional part of USARTDIV is represented with only 4 bits.  So
+	// if we use 9600 this will result in a small error of 0.02% (see pg 522).  Therefore
+	// the baud rate is actually 9,598.  Again, see pg 522.
+	ACCESS(USART2_BRR) |= 0x683;
+
+	// Enable USART2 for transmitting data
+	ACCESS(USART2_CR1) |= (1 << 3);
+
+	// Enable USART2
+	ACCESS(USART2_CR1) |= (1 << 13);
+
+	// Send text
+	SendString("Hello World\r\n");
+
+	while(1);
+}
+
